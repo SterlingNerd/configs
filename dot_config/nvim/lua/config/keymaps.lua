1c1be6f23cb5578f0af7cdf865bec4cf3b7b8a95
Josh <josh@sterling.digital>
2026-09-11 23:50:32 -0700

    fix: use <C-r>+ for insert mode clipboard paste instead of literal '+p

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,10 +2,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Ctrl+C/V for copy/paste
+-- Ctrl+C for copy (Visual mode)
 vim.keymap.set("v", "<C-c>", '"+y', { desc = "Copy to clipboard" })
-vim.keymap.set({ "n", "i" }, "<C-v>", '"+p', { desc = "Paste from clipboard" })
-
--- Shift+Ctrl+C/V also work
 vim.keymap.set("v", "<C-S-c>", '"+y', { desc = "Copy to clipboard" })
-vim.keymap.set({ "n", "i" }, "<C-S-v>", '"+p', { desc = "Paste from clipboard" })
+
+-- Ctrl+V for paste (Normal mode uses p, Insert mode uses <C-r>)
+vim.keymap.set("n", "<C-v>", '"+p', { desc = "Paste from clipboard" })
+vim.keymap.set("i", "<C-v>", '<C-r>+', { desc = "Paste from clipboard in insert mode" })
+
+-- Shift+Ctrl+V for paste
+vim.keymap.set("n", "<C-S-v>", '"+p', { desc = "Paste from clipboard" })
+vim.keymap.set("i", "<C-S-v>", '<C-r>+', { desc = "Paste from clipboard in insert mode" })
